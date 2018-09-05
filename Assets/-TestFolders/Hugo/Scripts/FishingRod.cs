deca42aef9dbf350f3c8af1f1bdbaa194c223f02
Hugo Cremer <slasken49@gmail.com>
2018-09-06 00:43:28 +1400

    fishing rod progress sort of

---
+++ Assets/-TestFolders/Hugo/Scripts/FishingRod.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FishingRod : MonoBehaviour
+{
+    public GameObject bob;
+    public Vector3 throwPoint;
+    public GameObject bobHolder;
+
+	public void ThrowBob (Vector3 direction)
+    {
+        GameObject newBob = Instantiate(bob, throwPoint, transform.rotation);
+        newBob.GetComponent<Rigidbody>().AddForce(direction);
+    }
+}
