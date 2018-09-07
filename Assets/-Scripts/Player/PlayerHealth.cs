4db95285e325644cb8af079ce24c1c36d7595e87
Jinexsus <sebi.halo@hotmail.com>
2018-09-07 10:35:47 +0200

    Sebbe
    changes to my scene and put some scripts in folders

---
+++ Assets/-Scripts/Player/PlayerHealth.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+
+public class PlayerHealth : MonoBehaviour
+{
+    public int startingHealth = 100;
+    public int currentHealth;
+
+    void Awake()
+    {
+        currentHealth = startingHealth;
+    }
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Death();
+        }
+    }
+
+
+    void Death()
+    {
+        //TODO: death screen/behaiviour.
+    }
+
+}
