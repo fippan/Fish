4b034edb3fd601c1998fd47db6c8b2e8550abf89
Filip Nilsson <filip_nilsson28@hotmail.com>
2018-09-11 09:19:16 +0200

    Started on GameManager and GameState stack system

---
+++ Assets/-TestFolders/fippan/Scripts/GameStateBase.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class GameStateBase : MonoBehaviour
+{
+
+	public abstract void OnEnterState();
+	public abstract void OnExitState();
+
+	public virtual void OnPauseState() { }
+	public virtual void OnUnPauseState() { }
+}
---
+++ Assets/-TestFolders/fippan/Scripts/MenuState.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuState : GameStateBase
+{
+	public override void OnEnterState()
+	{
+
+	}
+
+	public override void OnExitState()
+	{
+
+	}
+
+	public override void OnPauseState()
+	{
+		base.OnPauseState();
+
+
+	}
+}
