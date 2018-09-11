32712f227fcbc8ca536a1d5caf8f0d2a021e4e5f
Jim Andersson <andersson.jim@outlook.com>
2018-09-11 14:02:52 +0200

    Weapon and projectile scripts. Added a 1 handed musket pistol for testing.

---
+++ Assets/-TestFolders/Jim/Scripts/Projectile.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class Projectile : MonoBehaviour
+{
+    [HideInInspector] public float damage;
+    [HideInInspector] public float range;
+    [HideInInspector] public float speed;
+    [HideInInspector] public bool explosive;
+    [HideInInspector] public float explosionRadius;
+    [HideInInspector] public ParticleSystem onHitEffect;
+    [HideInInspector] public ParticleSystem trail;
+
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        Collider[] colliders = GetComponents<Collider>();
+        foreach (var item in colliders)
+        {
+            item.isTrigger = true;
+        }
+        rb.useGravity = false;
+        rb.velocity = Vector3.forward * speed;
+        if (trail != null) Instantiate(trail, transform);
+        Invoke("OnRangeReached", range);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        CancelInvoke();
+        if (onHitEffect != null) Destroy(Instantiate(onHitEffect, transform.position, Quaternion.identity), 3f);
+        if (explosive) Explode();
+        else
+        {
+            ICanTakeDamage target = other.GetComponentInParent<ICanTakeDamage>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+        Destroy(gameObject);
+    }
+
+    private void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (var item in colliders)
+        {
+            ICanTakeDamage target = item.GetComponentInParent<ICanTakeDamage>();
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+        }
+
+    }
+
+    private void OnRangeReached()
+    {
+        if (explosive) Explode();
+        else Destroy(gameObject);
+    }
+}
---
+++ Assets/-TestFolders/Jim/Scripts/Weapon.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [Header("Weapon attributes.")]
+    public float damage;
+    [Tooltip("Minimum time between shots in seconds.")]
+    public float fireingRate;
+    public float shotsUntilReload;
+    [Tooltip("Time in seconds.")]
+    public float reloadTime;
+    [Tooltip("Time in seconds until destroyed automatically.")]
+    public float range;
+    public float speed;
+
+    [Header("Explosion settings.")]
+    public bool explosive;
+    public float explosionRadius;
+
+    [Header("Projectile settings.")]
+    public Projectile projectile;
+    public ParticleSystem onHitEffect;
+    public float onHitEffectLifetime;
+    public ParticleSystem trail;
+
+    [Header("Barrel")]
+    public Transform barrelEnd;
+
+    private bool canFire = true;
+    private float shotsFired;
+    
+    public void Shoot()
+    {
+        if (!canFire) return;
+
+        if (canFire)
+        {
+            Fire();
+            shotsFired++;
+            canFire = false;
+            if (shotsFired > shotsUntilReload)
+            {
+                StartCoroutine(Reload());
+                return;
+            }
+            else
+            {
+                StartCoroutine(Cooldown());
+            }
+        }
+    }
+
+    private void Fire()
+    {
+        Projectile newProjectile = Instantiate(projectile, barrelEnd.position, barrelEnd.rotation);
+        newProjectile.damage = damage;
+        newProjectile.range = range;
+        newProjectile.speed = speed;
+        newProjectile.explosive = explosive;
+        newProjectile.explosionRadius = explosionRadius;
+        newProjectile.onHitEffect = onHitEffect;
+        newProjectile.trail = trail;
+    }
+
+    private IEnumerator Cooldown()
+    {
+        float timeSinceLastShot = 0;
+
+        while (timeSinceLastShot < fireingRate)
+        {
+            timeSinceLastShot += Time.deltaTime;
+            yield return null;
+        }
+
+        canFire = true;
+    }
+
+    private IEnumerator Reload()
+    {
+        float timeLeft = reloadTime;
+
+        while (timeLeft > 0)
+        {
+            timeLeft -= Time.deltaTime;
+            yield return null;
+        }
+
+        shotsFired = 0;
+        canFire = true;
+    }
+}
