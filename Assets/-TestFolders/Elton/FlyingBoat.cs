1794a5decedc1719d0d1cc01d2dda31a19fc280f
eltonr98 <31646723+eltonr98@users.noreply.github.com>
2018-09-11 14:07:55 +0200

    FlyingBoat

---
+++ Assets/-TestFolders/Elton/FlyingBoat.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class FlyingBoat : MonoBehaviour {
+    [SerializeField] private float timeCounter;
+
+    [SerializeField] private float speed;
+    [SerializeField] private float width;
+    [SerializeField] private float length;
+
+    public GameObject boat;
+
+
+
+	void Update () {
+        timeCounter += Time.deltaTime * speed;
+
+        float x = Mathf.Cos(timeCounter) * width;
+        float y = 0;
+        float z = Mathf.Sin(timeCounter) * length;
+
+        transform.position = boat.transform.position + new Vector3(x, y, z);
+
+
+        transform.Rotate(0,-0.05f, 0);
+	}
+}
