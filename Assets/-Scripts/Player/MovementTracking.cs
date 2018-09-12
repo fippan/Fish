29e2207a0287b884f1c0c6d02d7fa161fdb373b4
Filip Nilsson <filip_nilsson28@hotmail.com>
2018-09-12 13:25:54 +0200

    Player

---
+++ Assets/-Scripts/Player/MovementTracking.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementTracking : MonoBehaviour
+{
+	public Transform headProxy;
+
+	private void Update()
+	{
+		//Set rotation for head
+		transform.GetChild(0).transform.rotation = headProxy.transform.rotation;
+		transform.GetChild(1).transform.LookAt(transform.GetChild(0));
+
+		//Set position for body and head
+		transform.position = headProxy.transform.position;
+
+
+		//transform.rotation = headProxy.transform.rotation;
+	}
+}
---
+++ Assets/-Scripts/Player/Player.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour, ICanTakeDamage
+{
+    public float startingHealth = 100.0f;
+	public float currentHealth;
+
+    private void Start()
+    {
+        currentHealth = startingHealth;
+    }
+
+    private void Death()
+    {
+        //TODO: death screen/behaviour.
+        //Make enemies disapear and objects non usable - activate Watch menu and choose to restart or exit game
+    }
+
+    public void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+
+		if (currentHealth <= 0)
+            Death();
+    }
+}
