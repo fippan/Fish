82208f78679fa0b49dd650e1491b2be22fbd3536
Jim Andersson <andersson.jim@outlook.com>
2018-09-14 15:53:25 +0200

    Hit scan and automatic fire.

--- Assets/-TestFolders/Jim/Scripts/Weapon.cs
+++ Assets/-TestFolders/Jim/Scripts/Weapon.cs
@@ -5,52 +5,58 @@ using VRTK;
 public class Weapon : MonoBehaviour
 {
     [Header("Weapon attributes.")]
+    [Tooltip("Set true to enable hit scan weapon. False to shoot projectile.")]
+    public bool hitScan;
+    [Tooltip("Automatic fire when trigger is held down.")]
+    public bool automatic;
+    public bool spreadingBullets;
+    [Tooltip("Deal damage over an area.")]
+    public bool explosive;
+    [Space]
     public float damage;
-    [Tooltip("Minimum time between shots in seconds.")]
+    [Tooltip("Time between shots in seconds.")]
     public float firingRate;
+    public Vector2 minSpreadDegrees;
+    public Vector2 maxSpreadDegrees;
     public float shotsUntilReload;
     [Tooltip("Time in seconds.")]
     public float reloadTime;
-    [Tooltip("Time in seconds until destroyed automatically.")]
-    public float range;
-    public float speed;
 
     [Header("Explosion settings.")]
-    public bool explosive;
     public float explosionRadius;
 
     [Header("Projectile settings.")]
     public Projectile projectile;
+    [Tooltip("Time in seconds until destroyed automatically.")]
+    public float lifetime;
+    public float speed;
     public GameObject onShootEffect;
     public float onShootEffectLifetime;
     public GameObject onHitEffect;
     public float onHitEffectLifetime;
     public GameObject trail;
 
-    [Header("Haptic feedback.")]
-    [Range(0, 1)]
-    public float hapticStrenght;
-
     [Header("Barrel")]
     public Transform barrelEnd;
-
-    private VRTK_ControllerHaptics controllerHaptics;
+    
     private bool canFire = true;
+    private bool isTriggerDown = false;
     private float shotsFired;
-
-    private void Start()
-    {
-        controllerHaptics = GetComponent<VRTK_ControllerHaptics>();
-    }
 
     public void Shoot()
     {
+        if (automatic)
+        {
+            StartCoroutine(AutomaticFire());
+            return;
+        }
+
         if (!canFire) return;
 
         if (canFire)
         {
             Fire();
-            if (onShootEffect != null) Destroy(Instantiate(onShootEffect, barrelEnd.position, barrelEnd.rotation), onShootEffectLifetime);            
+            if (onShootEffect != null) Destroy(Instantiate(onShootEffect, barrelEnd.position, barrelEnd.rotation), onShootEffectLifetime);
             shotsFired++;
             canFire = false;
             if (shotsFired > shotsUntilReload)
@@ -65,18 +71,105 @@ public class Weapon : MonoBehaviour
         }
     }
 
-    public void HapticFeedback()
+    public void OnTriggerReleased()
     {
-        if (canFire)
-            VRTK_ControllerHaptics.TriggerHapticPulse
-                (VRTK_ControllerReference.GetControllerReference(transform.parent.gameObject), hapticStrenght);
+        isTriggerDown = false;
+    }
+
+    private IEnumerator AutomaticFire()
+    {
+        isTriggerDown = true;
+        while (isTriggerDown)
+        {
+            Fire();
+            if (onShootEffect != null) Destroy(Instantiate(onShootEffect, barrelEnd.position, barrelEnd.rotation), onShootEffectLifetime);
+            shotsFired++;
+            if (shotsFired > shotsUntilReload)
+            {
+                yield return StartCoroutine(Reload());
+            }
+            else
+            {
+                yield return new WaitForSeconds(firingRate);
+            }
+        }
     }
 
     private void Fire()
     {
-        Projectile newProjectile = Instantiate(projectile, barrelEnd.position, barrelEnd.rotation);
+        if (hitScan) FireWithHitScan();
+        else FireProjectile();
+    }
+
+    private void FireWithHitScan()
+    {
+        if (spreadingBullets)
+        {
+            Vector3 direction = CalculateHitScanDirection();
+            RaycastHit hit;
+            if (Physics.Raycast(barrelEnd.position, direction, out hit, 100f))
+                TargetHit(hit.transform, hit.point);
+        }
+        else
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(barrelEnd.position, barrelEnd.forward, out hit, 100f))
+                TargetHit(hit.transform, hit.point);
+        }
+    }
+
+    private Vector3 CalculateHitScanDirection()
+    {
+        Quaternion barrelEndStartRotation = barrelEnd.rotation;
+        Vector3 newRotation = new Vector3(
+            Random.Range(minSpreadDegrees.x, maxSpreadDegrees.x),
+            Random.Range(minSpreadDegrees.y, maxSpreadDegrees.y),
+            0f);
+        barrelEnd.Rotate(newRotation);
+        Vector3 direction = barrelEnd.forward;
+        barrelEnd.rotation = barrelEndStartRotation;
+
+        return direction;
+    }
+
+    private void TargetHit(Transform target, Vector3 point)
+    {
+        ICanTakeDamage targetHit = target.GetComponentInParent<ICanTakeDamage>();
+        if (targetHit != null)
+        {
+            targetHit.TakeDamage(damage);
+        }
+
+        if (onHitEffect != null)
+        {
+            Destroy(Instantiate(onHitEffect, point, Quaternion.identity), onHitEffectLifetime);
+        }
+    }
+
+    private void FireProjectile()
+    {
+        if (projectile == null)
+        {
+            Debug.LogWarning("Please assign a projectile in the inspector.");
+            return;
+        }        
+
+        if (spreadingBullets)
+        {
+            Projectile newProjectile = Instantiate(projectile, barrelEnd.position, CalculateProjectileRotation());
+            SetProjectileValues(newProjectile);
+        }
+        else
+        {
+            Projectile newProjectile = Instantiate(projectile, barrelEnd.position, barrelEnd.rotation);
+            SetProjectileValues(newProjectile);
+        }
+    }
+
+    private void SetProjectileValues(Projectile newProjectile)
+    {
         newProjectile.damage = damage;
-        newProjectile.range = range;
+        newProjectile.range = lifetime;
         newProjectile.speed = speed;
         newProjectile.explosive = explosive;
         newProjectile.explosionRadius = explosionRadius;
@@ -84,28 +177,30 @@ public class Weapon : MonoBehaviour
         newProjectile.trail = trail;
     }
 
+    private Quaternion CalculateProjectileRotation()
+    {
+        Quaternion barrelEndStartRotation = barrelEnd.rotation;
+        Vector3 newRotation = new Vector3(
+            Random.Range(minSpreadDegrees.x, maxSpreadDegrees.x),
+            Random.Range(minSpreadDegrees.y, maxSpreadDegrees.y),
+            0f);
+        barrelEnd.Rotate(newRotation);
+        Quaternion rotation = barrelEnd.rotation;
+        barrelEnd.rotation = barrelEndStartRotation;
+
+        return rotation;
+    }
+
     private IEnumerator Cooldown()
     {
-        float timeSinceLastShot = 0;
-
-        while (timeSinceLastShot < firingRate)
-        {
-            timeSinceLastShot += Time.deltaTime;
-            yield return null;
-        }
+        yield return new WaitForSeconds(firingRate);
 
         canFire = true;
     }
 
     private IEnumerator Reload()
     {
-        float timeLeft = reloadTime;
-
-        while (timeLeft > 0)
-        {
-            timeLeft -= Time.deltaTime;
-            yield return null;
-        }
+        yield return new WaitForSeconds(reloadTime);
 
         shotsFired = 0;
         canFire = true;
