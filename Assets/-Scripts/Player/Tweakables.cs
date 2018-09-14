ca729a28fb4b40cf2ffa51e526635569dfb58840
Filip Nilsson <filip_nilsson28@hotmail.com>
2018-09-14 15:58:23 +0200

    Added Ocean shader to project

--- Assets/-Scripts/Player/Tweakables.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-//[CreateAssetMenu]
-public class Tweakables : ScriptableObject
-{
-	[Header("Player vars")]
-	public float startingHealth = 100.0f;
-	public float speed = 5.0f;
-}
