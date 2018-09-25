d371215814cf345aaef1191614d86a33e7efc769
Jim Andersson <andersson.jim@outlook.com>
2018-09-25 10:32:03 +0200

    Rifle 01

---
+++ Assets/-TestFolders/Jim/Scripts/BulletTrace.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class BulletTrace : MonoBehaviour
+{
+    [SerializeField] LineRenderer linePrefab;
+    [SerializeField] float lifetime = .1f;
+
+    public void BulletLine(Vector3 start, Vector3 end)
+    {
+            LineRenderer newLine = Instantiate(linePrefab, Vector3.zero, Quaternion.identity);
+            Vector3[] points = new Vector3[2];
+            points[0] = start;
+            points[1] = end;
+            newLine.SetPositions(points);
+            Destroy(newLine.gameObject, lifetime);
+    }
+}
