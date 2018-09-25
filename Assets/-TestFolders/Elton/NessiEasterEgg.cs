a7b51c9c6120c10a2bdbba13a7af6165c4ca3ac9
eltonr98 <31646723+eltonr98@users.noreply.github.com>
2018-09-25 15:46:29 +0200

    sounds + nessi

---
+++ Assets/-TestFolders/Elton/NessiEasterEgg.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NessiEasterEgg : Health {
+    [SerializeField] private GameObject nessi;
+    protected override void Start()
+    {
+        base.Start();
+    }
+
+    protected override void Death()
+    {
+        Instantiate(nessi,transform);
+        Destroy(gameObject);
+    }
+}
