fb8a2a91162c5204298c7d0050fb8ecdf309d826
Jim Andersson <andersson.jim@outlook.com>
2018-09-25 13:23:22 +0200

    Reset weapon position when transform.position.y < 0

---
+++ Assets/-TestFolders/Jim/Scripts/ResetObjectPosition.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+
+public class ResetObjectPosition : MonoBehaviour
+{
+    private Transform[] resetPositions;
+    private Rigidbody rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        Transform positions = GameObject.Find("ResetPositions").transform;
+        resetPositions = new Transform[positions.childCount];
+        for (int i = 0; i < positions.childCount; i++)
+        {
+            resetPositions[i] = positions.GetChild(i);
+        }
+    }
+
+    private void Update()
+    {
+        if (transform.position.y < 0f)
+        {
+            ResetPositions();
+        }
+    }
+
+    //private void OnCollisionEnter(Collision collision)
+    //{
+    //    if (collision.transform.tag == "Water")
+    //    {
+    //        ResetPositions();
+    //    }
+    //}
+
+    private void ResetPositions()
+    {
+        rb.velocity = Vector3.zero;
+        int i = Random.Range(0, resetPositions.Length);
+        transform.position = resetPositions[i].position;
+    }
+}
