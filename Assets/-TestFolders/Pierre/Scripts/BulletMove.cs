062b1a32bd73c7813f2f2d7e6e7010898722eeb3
Unknown <pierrejuopperi@gmail.com>
2018-09-26 10:49:44 +0200

    Submarine enemy can now damage player + divers.

--- Assets/-TestFolders/Pierre/Scripts/BulletMove.cs
+++ Assets/-TestFolders/Pierre/Scripts/BulletMove.cs
@@ -3,9 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletMove : MonoBehaviour {
-	
+
+    [SerializeField]
+    private float damage;
 	// Update is called once per frame
 	void Update () {
-        transform.position += transform.forward * 0.5f;
+        transform.position += transform.forward * 0.35f;
 	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Health target = other.GetComponentInParent<Health>();
+        if (target != null)
+        {
+            if(target != target.GetComponent<Submarine>())
+            target.TakeDamage(damage, transform.position);
+        }
+    }
 }
