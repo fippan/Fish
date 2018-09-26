ba85e41e005fcf8b7978702664c0ffba6b19e8a5
Hugo Cremer <slasken49@gmail.com>
2018-09-27 02:57:06 +1400

    Teleport

--- Assets/-TestFolders/Pierre/Scripts/Teleport.cs
+++ Assets/-TestFolders/Pierre/Scripts/Teleport.cs
@@ -3,6 +3,14 @@
 public class Teleport : MonoBehaviour
 {
     [SerializeField] private Transform VRTK_Root;
+    [SerializeField] private Transform cameraEye;
+
+    public void SetDestination()
+    {
+        Vector3 pos = new Vector3(-0.13f, 0f, 0f);
+        VRTK_Root.position = pos;
+        VRTK_Root.rotation = Quaternion.Euler(new Vector3(0f, cameraEye.rotation.y, 0f));
+    }
 
     public void SetDestination(Vector3 position)
     {
